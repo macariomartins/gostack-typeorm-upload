f9632d953c11ed923b415ba361ad79cfab8eb8c7
Macário Martins <macariomartinsjunior@gmail.com>
2020-08-29 17:11:03 -0300

    Add transactions delete func

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,17 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(transactionId: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const transaction = await transactionsRepository.findOne(transactionId);
+
+    if (!transaction) throw new AppError('Transaction could not be found');
+
+    await transactionsRepository.delete(transactionId);
   }
 }
 
