093229a5568c0eaa59b9c174c8ccd7f2fc9966c1
Macário Martins <macariomartinsjunior@gmail.com>
2020-08-29 15:14:40 -0300

    Add decorations to Models

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,27 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import Category from "./Category";
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @ManyToOne(() => Category)
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @CreateDateColumn()
   updated_at: Date;
 }
 
