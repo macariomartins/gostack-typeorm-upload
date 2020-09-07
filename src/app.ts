67ad5cefefe1da2b3933b29302f8d387bd9685ec
Macário Martins <macariomartinsjunior@gmail.com>
2020-09-07 15:27:13 -0300

    Add categories in transactions listings

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
@@ -8,7 +9,7 @@ import routes from './routes';
 
 createConnection();
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
